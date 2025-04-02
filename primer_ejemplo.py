979def66dff894c91c8ce9413e626079c18bffbc
josvillanueva <A01737550@tec.mx>
2025-04-01 18:34:43 -0600

    Agregar funcion de suma

--- primer_ejemplo.py
+++ primer_ejemplo.py
@@ -2,3 +2,8 @@ print("Hola a todos esto es un repositorio global y se desplegara en githun")
 print("Este archivo se estra modificndo ")
 for i in range(10):
     print(i)
+
+def suma(a,b):
+    return a + b
+
+print(suma(2,2))
